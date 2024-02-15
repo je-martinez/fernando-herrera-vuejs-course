748ec4669f4e524556f4818eafe8a69a0ecb36fc
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-15 09:30:00 -0600

    chore: wip course

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
-  template: `
-  <h1>Hello, World!</h1>
-  <p>It's a beautiful day.</p>`,
+  //   template: `
+  //   <h1>Hello, World!</h1>
+  //   <p>It's a beautiful day.</p>`,
+  //   methods: {},
+  //   watch: {},
+  //   setup: {},
+
+  data() {
+    return {
+      title: "Hello, World!",
+      message: "It's a beautiful day.",
+    };
+  },
 });
 
 app.mount("#myApp");
