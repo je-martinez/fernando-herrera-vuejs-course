d3cbd5f5510e29469c4f35484582a91047c11c75
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-16 10:12:56 -0600

    test: wip unit testing

--- fundamentos/tests/unit/components/CounterComponent.spec.js
+++ fundamentos/tests/unit/components/CounterComponent.spec.js
@@ -2,6 +2,18 @@ import { shallowMount } from "@vue/test-utils";
 import Counter from "@/components/CounterComponent.vue";
 
 describe("Counter.vue", () => {
+  it("renders props.msg be default value", () => {
+    const msg = undefined;
+    const wrapper = shallowMount(Counter, {
+      props: {
+        title: msg,
+      },
+    });
+    const title = wrapper.find("h2");
+    expect(title.exists()).toBeTruthy();
+    expect(title.text()).toBe("Counter");
+  });
+
   it("renders props.title when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(Counter, {
@@ -9,6 +21,9 @@ describe("Counter.vue", () => {
         title: msg,
       },
     });
-    expect(wrapper.text()).toMatch(msg);
+
+    expect(wrapper.html()).toMatchSnapshot(msg);
+
+    // expect(wrapper.text()).toMatch(msg);
   });
 });
