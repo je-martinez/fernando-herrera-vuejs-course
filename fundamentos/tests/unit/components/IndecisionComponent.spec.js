5f00f6b4f984dceb35b38f10976fc12c4878241b
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-20 17:11:19 -0600

    test: wip

--- fundamentos/tests/unit/components/IndecisionComponent.spec.js
+++ fundamentos/tests/unit/components/IndecisionComponent.spec.js
@@ -10,4 +10,16 @@ describe("IndecisionComponent.vue", () => {
   it("should match with snapshot default value", () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
+
+  it("should not trigger nothing on typing", async () => {
+    const input = wrapper.find("input");
+    await input.setValue("holis");
+    const spyGetAnswer = jest.spyOn(wrapper.vm, "getAnswer");
+    expect(spyGetAnswer).not.toHaveBeenCalled();
+  });
+
+  it("should trigger api call if '?' was typed", async () => {});
+
+  it("should execute getAnswer on success", async () => {});
+  it("should execute getAnswer on failed", async () => {});
 });
