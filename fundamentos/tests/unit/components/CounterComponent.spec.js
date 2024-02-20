dce080ae409a1861d3e0c6d8f0e454c3bb335411
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-20 16:10:40 -0600

    chore: wip course

--- fundamentos/tests/unit/components/CounterComponent.spec.js
+++ fundamentos/tests/unit/components/CounterComponent.spec.js
@@ -2,20 +2,21 @@ import { shallowMount } from "@vue/test-utils";
 import Counter from "@/components/CounterComponent.vue";
 
 describe("Counter.vue", () => {
+  const wrapper = shallowMount(Counter);
   it("renders props.msg be default value", () => {
-    const msg = undefined;
-    const wrapper = shallowMount(Counter, {
-      props: {
-        title: msg,
-      },
-    });
+    // const msg = undefined;
+    // const wrapper = shallowMount(Counter, {
+    //   props: {
+    //     title: msg,
+    //   },
+    // });
     const title = wrapper.find("h2");
     expect(title.exists()).toBeTruthy();
     expect(title.text()).toBe("Counter");
   });
 
   it("should second <p> tag have default value", () => {
-    const wrapper = shallowMount(Counter);
+    // const wrapper = shallowMount(Counter);
 
     // const texts = wrapper.findAll("p");
     // expect(texts[1].text()).toBe("0");
@@ -24,21 +25,8 @@ describe("Counter.vue", () => {
     expect(text.text()).toBe("0");
   });
 
-  it("renders props.title when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(Counter, {
-      props: {
-        title: msg,
-      },
-    });
-
-    expect(wrapper.html()).toMatchSnapshot(msg);
-
-    // expect(wrapper.text()).toMatch(msg);
-  });
-
   it("should increment the counter", async () => {
-    const wrapper = shallowMount(Counter);
+    // const wrapper = shallowMount(Counter);
     const incrementButton = wrapper.find("#increment-btn");
     await incrementButton.trigger("click");
     const text = wrapper.find(`[data-testid="counter"]`);
@@ -46,10 +34,10 @@ describe("Counter.vue", () => {
   });
 
   it("should decrement the counter", async () => {
-    const wrapper = shallowMount(Counter);
+    // const wrapper = shallowMount(Counter);
     const decrementButton = wrapper.find("#decrement-btn");
     await decrementButton.trigger("click");
     const text = wrapper.find(`[data-testid="counter"]`);
-    expect(text.text()).toBe("-1");
+    expect(text.text()).toBe("0");
   });
 });
