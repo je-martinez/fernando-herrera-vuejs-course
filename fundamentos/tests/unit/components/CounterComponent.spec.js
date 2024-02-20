97de9121e236884cd9ca94f269b5f507ae75e265
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-20 15:13:48 -0600

    chore: continue course

--- fundamentos/tests/unit/components/CounterComponent.spec.js
+++ fundamentos/tests/unit/components/CounterComponent.spec.js
@@ -14,6 +14,16 @@ describe("Counter.vue", () => {
     expect(title.text()).toBe("Counter");
   });
 
+  it("should second <p> tag have default value", () => {
+    const wrapper = shallowMount(Counter);
+
+    // const texts = wrapper.findAll("p");
+    // expect(texts[1].text()).toBe("0");
+
+    const text = wrapper.find(`[data-testid="counter"]`);
+    expect(text.text()).toBe("0");
+  });
+
   it("renders props.title when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(Counter, {
