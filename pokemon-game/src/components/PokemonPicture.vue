535c5bbc46900da2ff4ad33beade0ce93d931bcc
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-21 13:03:47 -0600

    chore: vue course

---
+++ pokemon-game/src/components/PokemonPicture.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="pokemon-container">
+    <img :src="imgSrc" class="hidden-pokemon" alt="pokemon" />
+    <img :src="imgSrc" alt="pokemon" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pokemon-container {
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+img {
+  height: 200px;
+  position: absolute;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+</style>
---
+++ pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>¿Quien es este pokemon?</h1>
+  <pokemon-picture :pokemonId="35"></pokemon-picture>
+  <pokemon-options></pokemon-options>
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import PokemonOptions from "@/components/PokemonOptions.vue";
+
+export default {
+  components: { PokemonPicture, PokemonOptions },
+};
+</script>
+
+<style></style>
