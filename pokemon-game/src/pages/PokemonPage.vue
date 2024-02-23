5c0786b79731f50a460e6a5da20f661a2adf8b63
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-23 11:13:34 -0600

    chore: wip vuejs course

--- pokemon-game/src/pages/PokemonPage.vue
+++ pokemon-game/src/pages/PokemonPage.vue
@@ -1,15 +1,30 @@
 <template>
   <h1>¿Quien es este pokemon?</h1>
-  <pokemon-picture :pokemonId="35" :showPokemon="true"></pokemon-picture>
-  <pokemon-options></pokemon-options>
+  <pokemon-picture :pokemonId="35" :showPokemon="true" />
+  <pokemon-options :pokemons="pokemonArr" />
 </template>
 
 <script>
 import PokemonPicture from "@/components/PokemonPicture.vue";
 import PokemonOptions from "@/components/PokemonOptions.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
 
 export default {
+  mounted() {
+    this.mixPokemonArray();
+  },
   components: { PokemonPicture, PokemonOptions },
+  data() {
+    return {
+      pokemonArr: [],
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      const tmp = await getPokemonOptions();
+      this.pokemonArr = [...tmp];
+    },
+  },
 };
 </script>
 
