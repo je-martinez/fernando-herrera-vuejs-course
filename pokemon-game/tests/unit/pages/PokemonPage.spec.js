75e362846af663450777dc5e007bef2725c2bfde
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-28 18:09:16 -0600

    chore: vue test

--- pokemon-game/tests/unit/pages/PokemonPage.spec.js
+++ pokemon-game/tests/unit/pages/PokemonPage.spec.js
@@ -1,6 +1,8 @@
 import PokemonPage from "@/pages/PokemonPage.vue";
 import { mount, shallowMount } from "@vue/test-utils";
 import { pokemonArrayMock } from "../mocks/pokemons.mock";
+import PokemonPicture from "@/components/PokemonPicture";
+import PokemonOptions from "@/components/PokemonOptions";
 
 describe("PokemonPage.vue", () => {
   let wrapper;
@@ -29,11 +31,34 @@ describe("PokemonPage.vue", () => {
     shallowMount(PokemonPage, {
       data() {
         return {
-          pokemonArray: pokemonArrayMock,
+          pokemonArr: pokemonArrayMock,
           pokemon: pokemonArrayMock[0],
         };
       },
     });
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test("should render PokemonPicture Component & PokemonOptions Component", () => {
+    const wrapper = shallowMount(PokemonPage, {
+      data() {
+        return {
+          pokemonArr: pokemonArrayMock,
+          pokemon: pokemonArrayMock[0],
+        };
+      },
+    });
+
+    const picture = wrapper.findComponent("pokemon-picture-stub");
+    const options = wrapper.findComponent("pokemon-options-stub");
+
+    expect(picture.exists()).toBeTruthy();
+    expect(options.exists()).toBeTruthy();
+
+    expect(Number(picture.attributes("pokemonid"))).toBe(1);
+    expect(Number(picture.props("pokemonId"))).toBe(1);
+
+    expect(options.props("pokemons")).toEqual(pokemonArrayMock);
+    expect(options.attributes("pokemons")).toBeTruthy();
   });
 });
