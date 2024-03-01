0465178973a5255852e59b33d06eb5489f1274db
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-02-29 21:10:48 -0600

    chore: vuejs pokemon test

--- pokemon-game/tests/unit/pages/PokemonPage.spec.js
+++ pokemon-game/tests/unit/pages/PokemonPage.spec.js
@@ -1,6 +1,6 @@
 import PokemonPage from "@/pages/PokemonPage.vue";
 import { mount, shallowMount } from "@vue/test-utils";
-import { pokemonArrayMock } from "../mocks/pokemons.mock";
+import { pokemonArrayMock, pokemonsMock } from "../mocks/pokemons.mock";
 import PokemonPicture from "@/components/PokemonPicture";
 import PokemonOptions from "@/components/PokemonOptions";
 
@@ -60,5 +60,28 @@ describe("PokemonPage.vue", () => {
 
     expect(options.props("pokemons")).toEqual(pokemonArrayMock);
     expect(options.attributes("pokemons")).toBeTruthy();
+  });
+
+  test("should checkAnswer method should return true", async () => {
+    const wrapper = shallowMount(PokemonPage, {
+      data() {
+        return {
+          pokemonArr: pokemonArrayMock,
+          pokemon: pokemonArrayMock[0],
+        };
+      },
+    });
+
+    await wrapper.vm.checkAnswer(1);
+    expect(wrapper.find("h2").exists()).toBeTruthy();
+    expect(wrapper.vm.showPokemon).toBe(true);
+    expect(wrapper.find("h2").text()).toBe(
+      `Correcto, es ${pokemonsMock[0].name}`
+    );
+
+    await wrapper.vm.checkAnswer(2);
+    expect(wrapper.find("h2").exists()).toBeTruthy();
+    expect(wrapper.vm.showPokemon).toBe(true);
+    expect(wrapper.find("h2").text()).toBe(`Oops, era ${pokemonsMock[0].name}`);
   });
 });
