0c8838246b4459110c62dbbdae6269ce226e8b62
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-03-22 17:13:56 -0600

    feat: no found page

--- routes-lifecycle/src/router/router.js
+++ routes-lifecycle/src/router/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import AboutPage from "../modules/pokemon/pages/About.page";
 import ListPage from "../modules/pokemon/pages/List.page";
 import PokemonPage from "../modules/pokemon/pages/Pokemon.page";
+import NoFound from "../modules/shared/pages/NoFound.page";
 
 const routes = [
   { path: "/", component: ListPage },
   { path: "/about", component: AboutPage },
   { path: "/id", component: PokemonPage },
+  { path: "/:pathMatch(.*)*", component: NoFound },
 ];
 
 const router = createRouter({
