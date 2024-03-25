9a4d49dfdd0674dc58c6c976867710ad3541698f
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-03-25 15:06:13 -0600

    refactor: lazy loading

--- routes-lifecycle/src/router/router.js
+++ routes-lifecycle/src/router/router.js
@@ -1,12 +1,28 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import AboutPage from "../modules/pokemon/pages/About.page";
-import ListPage from "../modules/pokemon/pages/List.page";
-import PokemonPage from "../modules/pokemon/pages/Pokemon.page";
-import NoFound from "../modules/shared/pages/NoFound.page";
+
+const AboutPage = () =>
+  import(
+    /* webpackChunkName: "about-page" */ "../modules/pokemon/pages/About.page.vue"
+  );
+const ListPage = () =>
+  import(
+    /* webpackChunkName: "list-page" */ "../modules/pokemon/pages/List.page"
+  );
+const PokemonPage = () =>
+  import(
+    /* webpackChunkName: "pokemon-page" */ "../modules/pokemon/pages/Pokemon.page"
+  );
+const NoFound = () =>
+  import(
+    /* webpackChunkName: "no-found-page" */ "../modules/shared/pages/NoFound.page"
+  );
 
 const routes = [
   { path: "/", component: ListPage },
-  { path: "/about", component: AboutPage },
+  {
+    path: "/about",
+    component: AboutPage,
+  },
   { path: "/id", component: PokemonPage },
   { path: "/:pathMatch(.*)*", component: NoFound },
 ];
